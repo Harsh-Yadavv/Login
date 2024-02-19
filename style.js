4d6ae8d4eeac7635b394554ccdc39b73b477d009
Harsh Yadav <145007304+Harsh-Yadavv@users.noreply.github.com>
2024-02-19 20:48:30 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    
+    window.location.href = "https://harsh-yadavv.github.io/My-Site-Portfolio-/";
+});
+
+// document.getElementById("first").addEventListener("click", function() {
+//     document.getElementById("").innerHTML = '<img src="" alt="Username Image">';
+// });
